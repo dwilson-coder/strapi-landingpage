768f59a5a3444511d7f2aa299b15afda13d59489
dwilson-coder <dwilson_wbs@protonmail.com>
2024-02-15 15:40:19 -0500

    added middleware

--- src/api/landing-page/routes/landing-page.js
+++ src/api/landing-page/routes/landing-page.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * landing-page router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::landing-page.landing-page');
+module.exports = createCoreRouter("api::landing-page.landing-page", {
+  config: {
+    find: {
+      middlewares: ["api::landing-page.landing-page-populate"],
+    },
+    findOne: {
+      middlewares: ["api::landing-page.landing-page-populate"],
+    },
+  },
+});
